922480889b46a061f6dbbb1fe8986d3ff0967d47
huqingrui <2496834649@qq.com>
2023-10-09 21:41:40 +0900

    rm unnecessary file

--- run_fish_track.sh
+++ run_fish_track.sh
@@ -4,12 +4,9 @@
 #SBATCH --gres=gpu:4
 ###SBATCH -x tesla
 ## #SBATCH -w aventador
-zsh
 bash
-source /home/q_hu/workspace6/opt/anaconda3/etc/profile.d/conda.sh
 # conda init bash
 conda init
 conda activate yolo8
 ## Execute the python script
-cd /home/q_hu/workspace6/MOT/ultralytics
-python /home/q_hu/workspace6/MOT/ultralytics/tests/fish_tracker.py
+python ultralytics/tests/fish_tracker.py

12c1f2ee513f7000fdb8519cf03ea363448e16a6
MakotoITOH <mac_itoh77@yahoo.co.jp>
2023-10-14 22:44:09 +0900

    makoto edit10.14

--- fishtracking/fish_tracker.py
+++ fishtracking/fish_tracker.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
+import glob
 
 # Load an official or custom model
 # model = YOLO('yolov8n.pt')  # Load an official Detect model
-model = YOLO('fishtracking/best.pt')  # Load an official Detect model
+model = YOLO('./best.pt')  # Load an official Detect model
 # model = YOLO('yolov8n-seg.pt')  # Load an official Segment model
 # model = YOLO('yolov8n-pose.pt')  # Load an official Pose model
 # model = YOLO('path/to/best.pt')  # Load a custom trained model
@@ -11,5 +12,9 @@ model = YOLO('fishtracking/best.pt')  # Load an official Detect model
 # results = model.track(source="https://youtu.be/Zgi9g1ksQHc", show=True)  # Tracking with default tracker
 # results = model.track(source="https://youtu.be/Zgi9g1ksQHc", show=True, tracker="bytetrack.yaml")  # Tracking with ByteTrack tracker
 # results = model.track(source="/home/q_hu/workspace6/MOT/ultralytics/videos/10-fish-part2-2016_raw.mp4", show=False, tracker="bytetrack.yaml", save_txt=True, save=True, save_conf=True)  # Tracking with ByteTrack tracker
-results = model.track(source="fishtracking/video/sample.mp4", show=False, tracker="bytetrack.yaml", save_txt=True, save=True, save_conf=True)  # Tracking with ByteTrack tracker
+
+files = glob.glob("./video/*.mp4")
+
+for path in files:
+    results = model.track(source=path, show=False, tracker="bytetrack.yaml", save_txt=True, save=True, save_conf=True)  # Tracking with ByteTrack tracker
 # print(results)
